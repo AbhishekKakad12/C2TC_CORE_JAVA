48e23c03027ef6c8982e812b1064882bed962cdb
SANTOSH <abhishekkakad12@gmail.com>
2022-10-19 12:55:23 +0530

    first commit

--- Lecture1/src/çom/capgemini/javademos/AssignmentOperator.java
+++ Lecture1/src/çom/capgemini/javademos/AssignmentOperator.java
@@ -4,6 +4,7 @@ public class AssignmentOperator {
 
 	public static void main(String[] args) {
 		int x = 4;
+		int z = 8;
 		//x += 5; // x = x + 5
 		//x -= 5; // x = x - 5
 		//x *= 5; // x = x * 5
